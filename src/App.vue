c68c129d3b0608af45876da0cb3ebc990ac69963
cy <chanwit.yimneam@hotmail.com>
2024-11-23 20:10:59 +0700

    poc for vocard🥰

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,102 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
-import { useDrag, useMove } from '@vueuse/gesture';
-import {  useMotion } from '@vueuse/motion';
+import { useSwipe } from '@vueuse/core/index.cjs';
+import { computed, ref } from 'vue';
 
-const swipeArea = ref<HTMLElement>();
+const container = ref<HTMLElement | null>(null)
+const target = ref<HTMLElement | null>(null)
+const containerWidth = computed(() => container.value?.offsetWidth)
+const left = ref('0')
+const opacity = ref(1)
+// const background = ref('palegreen') 
+const background = ref('white')
 
-const { set } = useMotion(swipeArea, {
-  x: 0,
-  y: 0,
-});
+const { isSwiping, lengthX, lengthY } = useSwipe(target, {
+  passive: false,
+  onSwipe: (e) => {
+    if (containerWidth.value) {
+      const length = Math.abs(lengthX.value)
+
+      if (lengthX.value < 0) {
+        left.value = `${length}px`
+        background.value = 'palegreen'
+        opacity.value = 1 - (length / containerWidth.value)
+      } else if (lengthX.value > 0) {
+        left.value = `-${length}px`
+        background.value = 'red'
+        opacity.value = 1 - (length / containerWidth.value)
+      } else {
+        left.value = '0'
+        background.value = 'white'
+        opacity.value = 1
+      }
 
 
-// Ensure the drag gesture is initialized after the DOM is ready
-useMove(({offset, last, event}) => {
-  if (!offset) return;
-  const boxRect = swipeArea.value?.getBoundingClientRect()
-  
-  const [offsetX, offsetY] = offset;
-  if (!boxRect) return;
-  const x = event.pageX - boxRect.left - boxRect.width / 2
-  const y = event.pageY - boxRect.top - boxRect.height / 2
+    }
+  },
+  onSwipeEnd: (e, direction) => {
 
-
-  // console.log(`x: ${x}, y: ${y}`);
-  set({x, y})
-
-  if (last) {
-    if (offsetX > 50) {
-      console.log('Swiped Right');
-    } else if (offsetX < -50) {
-      console.log('Swiped Left');
+    if (lengthX.value < 0 && containerWidth.value && (Math.abs(lengthX.value) / containerWidth.value) > 0.5) {
+      // background.value = 'palegreen'
+      left.value = `${containerWidth.value}px`
+      opacity.value = 0
+    } else if (lengthX.value > 0 && containerWidth.value && (Math.abs(lengthX.value) / containerWidth.value) > 0.5) {
+      left.value = `-${containerWidth.value}px`
+      opacity.value = 0
+    } else {
+      left.value = '0'
+      background.value = 'white'
+      opacity.value = 1
     }
   }
-}, {
-  domTarget: swipeArea,
-});
+})
+
 </script>
 
-
 <template>
-  <div ref="swipeArea" class="swipe-area" >
-    Swipe Here
+  <div>
+    <div ref="container" class="container select-none">
+      <div ref="target" class="overlay" :class="{ animated: !isSwiping }" :style="{ left, opacity, background }">
+        <p>Target</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-.swipe-area {
-  background-color: red;
-  width: 50px;
-  height: 50px;
-  /* background-color: #f0f0f0; */
+.container {
+  position: relative;
   display: flex;
-  justify-content: center;
   align-items: center;
-  /* user-select: none; */
-  /* Disable text selection */
-  /* touch-action: none; */
-  /* Disable default browser gestures */
+  justify-content: center;
+  border: 6px dashed pink;
+  border-radius: 15px;
+  overflow: hidden;
+  height: 150px;
+}
+
+.overlay {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background: white;
+}
+
+.overlay.animated {
+  transition: all 0.2s ease-in-out;
+}
+
+.overlay>p {
+  padding: 10px;
+  font-size: 20px;
+  font-weight: bold;
+  color: black;
+  overflow: hidden;
+  letter-spacing: 2px;
+  white-space: nowrap;
 }
 </style>
